5c79870e91491108e60fb8a64a58f400692ad0a1
Tony Stenberg <69804459+tonystenberg-ou@users.noreply.github.com>
2022-10-31 22:17:23 +0100

    Add tmux cheat sheets

--- nvim-lua/.config/nvim/after/plugin/lsp.lua
+++ nvim-lua/.config/nvim/after/plugin/lsp.lua
@@ -2,7 +2,7 @@ local Remap = require("theprimeagen.keymap")
 local nnoremap = Remap.nnoremap
 local inoremap = Remap.inoremap
 
-local sumneko_root_path = "/home/mpaulson/personal/sumneko"
+local sumneko_root_path = "/home/linuxbrew/.linuxbrew/Cellar/lua-language-server/3.6.3"
 local sumneko_binary = sumneko_root_path .. "/bin/lua-language-server"
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
