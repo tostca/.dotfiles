7f58d8608c0de57d5d7d693dc6401a5f55d9b72d
Tony Stenberg <69804459+tonystenberg-ou@users.noreply.github.com>
2023-01-05 19:33:38 +0100

    fix: packer commands not found

--- nvim-simple/.config/nvim/lua/theprimeagen/init.lua
+++ nvim-simple/.config/nvim/lua/theprimeagen/init.lua
@@ -1,5 +1,6 @@
 require("theprimeagen.set")
 require("theprimeagen.remap")
+require("theprimeagen.packer")
 
 local augroup = vim.api.nvim_create_augroup
 local ThePrimeagenGroup = augroup('ThePrimeagen', {})
