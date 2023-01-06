f3a10fc24bb48d48073cdb644a4084c73c0c1b30
Tony Stenberg <tonystenberg@hey.com>
2023-01-06 01:01:52 +0100

    fix telescope function typo.

--- nvim-simple/.config/nvim/after/plugin/lsp.lua
+++ nvim-simple/.config/nvim/after/plugin/lsp.lua
@@ -125,7 +125,7 @@ lsp.set_preferences({
   }
 })
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(function(_, bufnr)
   local opts = { buffer = bufnr, remap = false }
 
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
@@ -136,8 +136,8 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
   vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
   vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {buffer = bufnr, desc = '[G]oto [R]eferences'})
-  vim.keymap.set("n", "ds", require("telescope.builtin").lsp_document_symols, {buffer = bufnr, desc = '[D]ocument [S]ymbols'})
-  vim.keymap.set("n", "<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symols, {buffer = bufnr, desc = '[W]orkspace [S]ymbols'})
+  vim.keymap.set("n", "<leader>ds", require("telescope.builtin").lsp_document_symbols, {buffer = bufnr, desc = '[D]ocument [S]ymbols'})
+  vim.keymap.set("n", "<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, {buffer = bufnr, desc = '[W]orkspace [S]ymbols'})
   vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
   vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end)
