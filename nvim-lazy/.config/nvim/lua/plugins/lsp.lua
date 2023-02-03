cc51144fb05a51b7404d6630ce275a753b7fcf9e
tostca <tony.stenberg@hey.com>
2023-02-03 15:32:44 +0100

    Configure lsp servers

---
+++ nvim-lazy/.config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,87 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        elixirls = {
+          settings = {
+            elixirLS = {
+              filetypes = { "elixir", "eelixir" },
+              dialyzerEnabled = false,
+              fetchDeps = false,
+              enableTestLenses = true,
+            }
+          }
+        },
+
+        html = {
+          filetypes = { 'html', 'heex', 'eex' },
+        },
+
+        emmet_ls = {
+          filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'heex', 'eex' }
+        },
+
+        tailwindcss = {
+          init_options = {
+            userLanguages = {
+              elixir = "phoenix-heex",
+              eruby = "erb",
+              heex = "phoenix-heex",
+              svelte = "html",
+            },
+          },
+          settings = {
+            includeLanguages = {
+              typescript = "javascript",
+              typescriptreact = "javascript",
+              ["html-eex"] = "html",
+              ["phoenix-heex"] = "html",
+              heex = "html",
+              eelixir = "html",
+              elm = "html",
+              erb = "html",
+              svelte = "html",
+            },
+            tailwindCSS = {
+              lint = {
+                cssConflict = "warning",
+                invalidApply = "error",
+                invalidConfigPath = "error",
+                invalidScreen = "error",
+                invalidTailwindDirective = "error",
+                invalidVariant = "error",
+                recommendedVariantOrder = "warning",
+              },
+              experimental = {
+                classRegex = {
+                  [[class= "([^"]*)]],
+                  [[class: "([^"]*)]],
+                  '~H""".*class="([^"]*)".*"""',
+                },
+              },
+              validate = true,
+            },
+          },
+          filetypes = {
+            "css",
+            "scss",
+            "sass",
+            "html",
+            "heex",
+            "elixir",
+            "eruby",
+            "javascript",
+            "javascriptreact",
+            "typescript",
+            "typescriptreact",
+            "svelte",
+          },
+        }
+      },
+    },
+  },
+}
