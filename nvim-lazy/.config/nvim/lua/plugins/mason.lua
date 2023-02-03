5aebe0aff7b138b0d8eae40c08913f1aec8eee41
tostca <tony.stenberg@hey.com>
2023-02-03 15:18:46 +0100

    Add languages to mason, add typescript support from extras config

---
+++ nvim-lazy/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,22 @@
+return {
+  "williamboman/mason.nvim",
+  opts = {
+    ensure_installed = {
+      "stylua",
+      "shellcheck",
+      "shfmt",
+      "flake8",
+      "css-lsp",
+      "elixir-ls",
+      "emmet-ls",
+      "eslint-lsp",
+      "graphql-language-service-cli",
+      "html-lsp",
+      "intelephense",
+      "json-lsp",
+      "lua-language-server",
+      "prettierd",
+      "tailwindcss-language-server",
+    },
+  }
+}
