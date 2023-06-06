0faf524047fc19c4b008e4a235f986653ee7ad91
tostca <tony.stenberg@hey.com>
2023-06-06 22:52:03 +0200

    Add elixir snippets and lazyVim extras

--- nvim-lazy/.config/nvim/lua/config/keymaps.lua
+++ nvim-lazy/.config/nvim/lua/config/keymaps.lua
@@ -6,3 +6,9 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set(
+  "n",
+  "<leader>rs",
+  "<cmd>source ~/.config/nvim/lua/snippets/elixir.lua<CR>",
+  { desc = "Reload Lua Snippets" }
+)
--- nvim-lazy/.config/nvim/lua/config/lazy.lua
+++ nvim-lazy/.config/nvim/lua/config/lazy.lua
@@ -13,13 +13,16 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.coding.copilot" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
+    { import = "lazyvim.plugins.extras.dap.core" },
+    { import = "lazyvim.plugins.extras.dap.nlua" },
     -- import/override with your plugins
     { import = "plugins" },
     { import = "langs" },
+    -- { import = "snippets" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
