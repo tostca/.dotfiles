3adffbc5fb8dc5a21835a28c85b163521471cdbd
tostca <tony.stenberg@hey.com>
2024-02-01 20:58:52 +0100

    change lualine separator

---
+++ nvim-lazy/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      opts.options = {
+        component_separators = "|",
+        section_separators = "",
+      }
+    end,
+  },
+}
