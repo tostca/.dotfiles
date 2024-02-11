6e10dcfb86769e2ef4056d46808d9a208f6f202e
tostca <tony.stenberg@hey.com>
2024-02-11 21:45:18 +0100

    decrease conceal for obsidian

--- nvim-lazy/.config/nvim/lua/config/options.lua
+++ nvim-lazy/.config/nvim/lua/config/options.lua
@@ -2,5 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.guicursor = ""
-vim.opt.scrolloff = 8
+local opt = vim.opt
+opt.guicursor = ""
+opt.scrolloff = 8
+
+opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
