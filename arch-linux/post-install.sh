0f12b83b1a8d7d605af9b079aa78b3d840440a9e
tostca <tony.stenberg@hey.com>
2024-03-04 09:04:11 +0100

    WIP: arch linux post install

---
+++ arch-linux/post-install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Update all packages
+pacman -Syu--noconfirm
+
+# Packages for development
+pacman -S --noconfirm git \
+	stow \
+	neovim \
+	vim \
+	fd \
+	lazygit \
+	docker \
+	tmux \
+	qmk \
+	deno \
+	ttf-nerd-fonts-symbols-mono \
+	ttf-jetbrains-mono-nerd \
+	ttf-victor-mono-nerd \
+	fzf \
+	coreutils \
+	curl \
+	wget \
+	bat \
+	autoconf \
+	openssl \
+	libxslt \
+	fop \
+	go \
+	go-tools \
+	sqlite \
+	jq \
+	yq \
+	ripgrep \
+	rofi \
+	dash \
+	imlib2 \
+	xorg-xsetroot \
+	feh \
+	acpi \
+	libx11 \
+	libxft \
+	libxinerama \
+	freetype2 \
+	fontconfig
