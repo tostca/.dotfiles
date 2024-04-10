84416412d0b07edb9c1ea3acb07fd0141624537d
tostca <tony.stenberg@hey.com>
2024-04-10 09:38:46 +0200

    Using latest kotlin language server from main temporarily for now.

--- nvim-lazy/.config/nvim/lua/langs/kotlin.lua
+++ nvim-lazy/.config/nvim/lua/langs/kotlin.lua
@@ -11,18 +11,18 @@ return {
       vim.list_extend(opts.ensure_installed, { "kotlin-debug-adapter", "gradle-language-server" })
     end,
   },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        kotlin_language_server = {
-          default_config = {
-            cmd = {
-              "/Users/tonystenberg/personal/kotlin-language-server/server/build/install/server/bin/kotlin-language-server",
-            },
-          },
-        },
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       kotlin_language_server = {
+  --         default_config = {
+  --           cmd = {
+  --             "/Users/tonystenberg/personal/kotlin-language-server/server/build/install/server/bin/kotlin-language-server",
+  --           },
+  --         },
+  --       },
+  --     },
+  --   },
+  -- },
 }
