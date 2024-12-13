bcb8d662260a30e43b8bcb4af650aa423414ce07
tostca <tony.stenberg@hey.com>
2024-12-13 17:42:53 +0100

    disable all animations in new snacks.nvim plugin

--- nvim-lazy/.config/nvim/lua/config/options.lua
+++ nvim-lazy/.config/nvim/lua/config/options.lua
@@ -8,6 +8,9 @@ opt.scrolloff = 8
 
 opt.conceallevel = 2 -- Hide * markup for bold and italic, but not markers with substitutions
 
+-- Disable all animations
+vim.g.snacks_animate = false
+
 vim.g.lazyvim_picker = "telescope"
 vim.g.lazyvim_python_lsp = "pyright"
 -- vim.g.lazyvim_python_lsp = "basedpyright"
