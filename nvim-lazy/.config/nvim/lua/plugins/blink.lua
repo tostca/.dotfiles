a355d261767d14e3c97039415741245b9fb5cb96
tostca <tony.stenberg@hey.com>
2024-12-13 17:43:15 +0100

    add more sane keymaps for completions in blink

---
+++ nvim-lazy/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,45 @@
+return {
+  {
+    "saghen/blink.cmp",
+    opts = {
+      keymap = {
+        preset = "enter",
+        ["<CR>"] = {},
+        ["<C-y>"] = { "select_and_accept" },
+      },
+    },
+  },
+  -- keys = {
+  --   -- disable the keymap to select
+  --   { "<CR>", false },
+  --
+  --   -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  --   ["<C-y>"] = require("cmp").mapping.confirm({ select = true }),
+  -- },
+  -- {
+  --   "saghen/blink.cmp",
+  --   version = "v0.*",
+  --   -- !Important! Make sure you're using the latest release of LuaSnip
+  --   -- `main` does not work at the moment
+  --   dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
+  --   opts = {
+  --     snippets = {
+  --       expand = function(snippet)
+  --         require("luasnip").lsp_expand(snippet)
+  --       end,
+  --       active = function(filter)
+  --         if filter and filter.direction then
+  --           return require("luasnip").jumpable(filter.direction)
+  --         end
+  --         return require("luasnip").in_snippet()
+  --       end,
+  --       jump = function(direction)
+  --         require("luasnip").jump(direction)
+  --       end,
+  --     },
+  --     sources = {
+  --       default = { "lsp", "path", "luasnip", "buffer" },
+  --     },
+  --   },
+  -- },
+}
