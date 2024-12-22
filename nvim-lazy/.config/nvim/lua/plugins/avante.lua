6a91ecb271059c45d979e7caa9cf6ab8179c8716
tostca <tony.stenberg@hey.com>
2024-12-22 15:08:20 +0100

    feat(ai-avante): change provider to copilot

--- nvim-lazy/.config/nvim/lua/plugins/avante.lua
+++ nvim-lazy/.config/nvim/lua/plugins/avante.lua
@@ -6,6 +6,17 @@ return {
     end,
     event = "VeryLazy",
     opts = {
+      provider = "copilot",
+      copilot = {
+        endpoint = "https://api.githubcopilot.com",
+        model = "gpt-4o-2024-08-06",
+        proxy = nil, -- [protocol://]host[:port] Use this proxy
+        allow_insecure = false, -- Allow insecure server connections
+        timeout = 30000, -- Timeout in milliseconds
+        temperature = 0,
+        max_tokens = 4096,
+      },
+      auto_suggestions_provider = "copilot",
       hints = { enabled = false },
       -- provider = "openai",
       -- openai = {
@@ -16,8 +27,37 @@ return {
       --   max_tokens = 4096,
       --   ["local"] = true,
       -- },
+      windows = {
+        position = "right",
+        width = 40,
+      },
     },
     build = LazyVim.is_win() and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
+    dependencies = {
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            -- use_absolute_path = true,
+          },
+        },
+      },
+    },
   },
   {
     "MeanderingProgrammer/render-markdown.nvim",
