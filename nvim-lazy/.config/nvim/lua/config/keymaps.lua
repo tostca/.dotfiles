4e16d3bde097aacaf84a39364c55fea70373544c
tostca <tony.stenberg@hey.com>
2024-12-22 15:09:07 +0100

    disable loading elixir snippets

--- nvim-lazy/.config/nvim/lua/config/keymaps.lua
+++ nvim-lazy/.config/nvim/lua/config/keymaps.lua
@@ -8,12 +8,12 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set(
-  "n",
-  "<leader>rs",
-  "<cmd>source ~/.config/nvim/lua/snippets/elixir.lua<CR>",
-  { desc = "Reload Lua Snippets" }
-)
+-- vim.keymap.set(
+--   "n",
+--   "<leader>rs",
+--   "<cmd>source ~/.config/nvim/lua/snippets/elixir.lua<CR>",
+--   { desc = "Reload Lua Snippets" }
+-- )
 
 -- yq commands to convert YML to JSON and JSON to YML.
 vim.keymap.set("n", "<leader>cj", "<cmd>%!yq -o=json '.'<CR>", { desc = "Convert YAML to JSON" })
