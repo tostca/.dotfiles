8ff2f9eff2cf1324bc3b0337203d628de4760cde
tostca <tony.stenberg@hey.com>
2024-12-31 12:15:06 +0100

    add toggle copilot and clean up

--- nvim-lazy/.config/nvim/lua/config/keymaps.lua
+++ nvim-lazy/.config/nvim/lua/config/keymaps.lua
@@ -26,33 +26,6 @@ vim.keymap.set("n", "<leader>uc", function()
   Util.toggle("conceallevel", false, { 0, conceallevel })
 end, { desc = "Toggle Conceal" })
 
--- Delete all buffers except current
--- local function delete_all_buffers_except_current()
---   local bufs = vim.api.nvim_list_bufs()
---   local current_buf = vim.api.nvim_get_current_buf()
---   for _, i in ipairs(bufs) do
---     if i ~= current_buf then
---       vim.api.nvim_buf_delete(i, {})
---     end
---   end
--- end
--- --
--- -- Delete all buffers
--- local function delete_all_buffers()
---   local bufs = vim.api.nvim_list_bufs()
---   for _, i in ipairs(bufs) do
---     vim.api.nvim_buf_delete(i, {})
---   end
--- end
---
--- vim.keymap.set("n", "<leader>bc", function()
---   delete_all_buffers_except_current()
--- end, { desc = "Delete all buffers but current" })
---
--- vim.keymap.set("n", "<leader>ba", function()
---   delete_all_buffers()()
--- end, { desc = "Delete all buffers" })
-
 -- Obsidian
 vim.keymap.set("n", "<leader>ot", "<cmd>ObsidianTags<CR>", { desc = "Obsidian find notes by tag" })
 vim.keymap.set("n", "<leader>on", "<cmd>ObsidianNew<CR>", { desc = "Obsidian create new note in inbox" })
@@ -62,3 +35,10 @@ vim.keymap.set("n", "<leader>od", "<cmd>ObsidianToday<CR>", { desc = "Obsidian c
 vim.keymap.set("n", "<leader>cw", "<cmd>windo diffthis<CR>", { desc = "Start diff for open windows" })
 -- Could also use <cmd>windo diffoff<CR>
 vim.keymap.set("n", "<leader>cW", "<cmd>diffoff!<CR>", { desc = "Stop diff for open windows" })
+
+-- Copilot
+vim.keymap.set("n", "<leader>ac", function()
+  require("copilot.suggestion").toggle_auto_trigger()
+  local state = vim.b.copilot_suggestion_auto_trigger
+  vim.notify("Copilot: Auto Trigger " .. (state and "Enabled" or "Disabled"))
+end, { desc = "Copilot toggle suggestion auto trigger" })
