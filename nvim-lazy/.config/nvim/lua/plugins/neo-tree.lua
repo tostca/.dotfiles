2425572c4486695b429e75317b8bfe2ebe258375
Tony Stenberg <tony.stenberg@hey.com>
2025-05-21 21:16:06 +0200

    add dotnet templates support

--- nvim-lazy/.config/nvim/lua/plugins/neo-tree.lua
+++ nvim-lazy/.config/nvim/lua/plugins/neo-tree.lua
@@ -5,5 +5,22 @@ return {
       position = "right",
       width = 240,
     },
+    filesystem = {
+      window = {
+        mappings = {
+          -- Make the mapping anything you want
+          ["T"] = "easy",
+        },
+      },
+      commands = {
+        ["easy"] = function(state)
+          local node = state.tree:get_node()
+          local path = node.type == "directory" and node.path or vim.fs.dirname(node.path)
+          require("easy-dotnet").create_new_item(path, function()
+            require("neo-tree.sources.manager").refresh(state.name)
+          end)
+        end,
+      },
+    },
   },
 }
