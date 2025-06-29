a3800671875883158f861d4b8a9133436359f5c2
Tony Stenberg <tony.stenberg@hey.com>
2025-06-29 10:52:04 +0200

    add dap breakpoint custom icons

--- nvim-lazy/.config/nvim/lua/config/options.lua
+++ nvim-lazy/.config/nvim/lua/config/options.lua
@@ -14,3 +14,30 @@ vim.g.snacks_animate = false
 -- vim.g.lazyvim_picker = "telescope"
 vim.g.lazyvim_python_lsp = "pyright"
 -- vim.g.lazyvim_python_lsp = "basedpyright"
+
+-- DAP breakpoint icons and colors
+-- vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "Error", linehl = "", numhl = "" })
+-- vim.fn.sign_define("DapBreakpointCondition", { text = "", texthl = "Function", linehl = "", numhl = "" })
+-- vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "Comment", linehl = "", numhl = "" })
+-- vim.fn.sign_define("DapStopped", { text = "🠶", texthl = "String", linehl = "DiffAdd", numhl = "" })
+vim.fn.sign_define("DapBreakpoint", { text = " ", texthl = "Error", linehl = "", numhl = "" })
+vim.fn.sign_define("DapBreakpointCondition", { text = " ", texthl = "Function", linehl = "", numhl = "" })
+vim.fn.sign_define("DapBreakpointRejected", { text = " ", texthl = "Comment", linehl = "", numhl = "" })
+vim.fn.sign_define("DapStopped", { text = "󰁕 ", texthl = "String", linehl = "DiffAdd", numhl = "" })
+vim.fn.sign_define("DapLogPoint", { text = ".>", texthl = "String", linehl = "DiffAdd", numhl = "" })
+-- local dap_icons = {
+--   Stopped = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
+--   Breakpoint = " ",
+--   BreakpointCondition = " ",
+--   BreakpointRejected = { " ", "DiagnosticError" },
+--   LogPoint = ".>",
+-- }
+-- for name, sign in pairs(dap_icons) do
+--   sign = type(sign) == "table" and sign or { sign }
+--   vim.fn.sign_define("Dap" .. name, {
+--     text = sign[1],
+--     texthl = sign[2] or "DiagnosticInfo",
+--     linehl = sign[3],
+--     numhl = sign[3],
+--   })
+-- end
