5e322bf84e1a41e7e21de686efd7013422a1dee3
Tony Stenberg <tony.stenberg@hey.com>
2025-08-05 04:44:56 +0200

    remove unused nvim plugins

--- nvim-lazy/.config/nvim/lua/plugins/tools.lua
+++ nvim-lazy/.config/nvim/lua/plugins/tools.lua
@@ -1,24 +1,4 @@
 return {
-  -- markdown preview
-  -- {
-  --   "toppair/peek.nvim",
-  --   build = "deno task --quiet build:fast",
-  --   keys = {
-  --     {
-  --       "<leader>op",
-  --       function()
-  --         local peek = require("peek")
-  --         if peek.is_open() then
-  --           peek.close()
-  --         else
-  --           peek.open()
-  --         end
-  --       end,
-  --       desc = "Peek (Markdown Preview)",
-  --     },
-  --   },
-  --   opts = { theme = "light" },
-  -- },
   {
     "jackMort/ChatGPT.nvim",
     cmd = { "ChatGPTActAs", "ChatGPT" },
