71c9f12cd96d341a0ae535b2453a59d700d79e86
Tony Stenberg <tony.stenberg@hey.com>
2025-09-18 15:44:57 +0200

    fix avante config for new version

--- nvim-lazy/.config/nvim/lua/plugins/avante.lua
+++ nvim-lazy/.config/nvim/lua/plugins/avante.lua
@@ -1,40 +1,50 @@
 return {
   {
     "yetone/avante.nvim",
-    init = function()
-      require("avante_lib").load()
-    end,
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- ⚠️ must add this setting! ! !
+    build = vim.fn.has("win32") ~= 0 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+      or "make",
     event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
     opts = {
-      debug = true,
-      provider = "ollama",
+      -- add any opts here
+      -- this file can contain specific instructions for your project
+      instructions_file = "avante.md",
+      -- for example
+      provider = "claude",
       providers = {
-        ollama = {
-          api_key_name = "",
-          endpoint = "http://127.0.0.1:11434",
-          model = "tony-qwen32",
+        claude = {
+          endpoint = "https://api.anthropic.com",
+          model = "claude-sonnet-4-20250514",
+          timeout = 30000, -- Timeout in milliseconds
           extra_request_body = {
-            options = {
-              num_ctx = 32768,
-              temperature = 0,
-            },
+            temperature = 0.75,
+            max_tokens = 20480,
           },
-          stream = true,
+        },
+        ollama = {
+          endpoint = "http://localhost:11434",
+          model = "qwen3-coder:30b",
         },
       },
-      auto_suggestions_provider = "ollama",
-      hints = { enabled = true },
       windows = {
         position = "right",
         width = 40,
       },
     },
-    build = LazyVim.is_win() and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" or "make",
     dependencies = {
-      "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
+      "nvim-mini/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "stevearc/dressing.nvim", -- for input provider dressing
+      "folke/snacks.nvim", -- for input provider snacks
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
       "zbirenbaum/copilot.lua", -- for providers='copilot'
       {
@@ -50,18 +60,19 @@ return {
               insert_mode = true,
             },
             -- required for Windows users
-            -- use_absolute_path = true,
+            use_absolute_path = true,
           },
         },
       },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        "MeanderingProgrammer/render-markdown.nvim",
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
     },
-  },
-  {
-    "MeanderingProgrammer/render-markdown.nvim",
-    optional = true,
-    ft = function(_, ft)
-      vim.list_extend(ft, { "Avante" })
-    end,
   },
   {
     "folke/which-key.nvim",
