5385ebcb23851c3c43e12fc0d98423620edb6f86
Tony Stenberg <tony.stenberg@hey.com>
2025-10-24 13:37:49 +0200

    formatting

--- nvim-lazy/.config/nvim/lua/langs/django.lua
+++ nvim-lazy/.config/nvim/lua/langs/django.lua
@@ -1,41 +1,41 @@
 return {
   -- add django syntax highlighting djlsp
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "htmldjango" })
-    end,
-  },
-  -- add djlsp
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        djlsp = {},
-      },
-    },
-  },
-  {
-    "mason-org/mason.nvim",
-    opts = { ensure_installed = { "djlint" } },
-  },
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        htmldjango = { "djlint" },
-        django = { "djlint" },
-        -- html = { "djlint" },
-      },
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = {
-        htmldjango = { "djlint" },
-        django = { "djlint" },
-      },
-    },
-  },
+  -- {
+  --   "nvim-treesitter/nvim-treesitter",
+  --   opts = function(_, opts)
+  --     vim.list_extend(opts.ensure_installed, { "htmldjango" })
+  --   end,
+  -- },
+  -- -- add djlsp
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       djlsp = {},
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "mason-org/mason.nvim",
+  --   opts = { ensure_installed = { "djlint" } },
+  -- },
+  -- {
+  --   "mfussenegger/nvim-lint",
+  --   opts = {
+  --     linters_by_ft = {
+  --       htmldjango = { "djlint" },
+  --       django = { "djlint" },
+  --       -- html = { "djlint" },
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   opts = {
+  --     formatters_by_ft = {
+  --       htmldjango = { "djlint" },
+  --       django = { "djlint" },
+  --     },
+  --   },
+  -- },
 }
