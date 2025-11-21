17d989c849114af62540d0db87f8ded94f8e49c2
Tony Stenberg <tony.stenberg@hey.com>
2025-11-21 09:55:18 +0100

    add easy-dotnet shortcut

--- nvim-lazy/.config/nvim/lua/plugins/snacks.lua
+++ nvim-lazy/.config/nvim/lua/plugins/snacks.lua
@@ -9,6 +9,28 @@ return {
       sources = {
         explorer = {
           layout = { layout = { position = "right" } },
+          win = {
+            list = {
+              keys = {
+                ["A"] = "explorer_add_dotnet",
+              },
+            },
+          },
+          actions = {
+            explorer_add_dotnet = function(picker)
+              local dir = picker:dir()
+              local easydotnet = require("easy-dotnet")
+
+              easydotnet.create_new_item(dir, function(item_path)
+                local tree = require("snacks.explorer.tree")
+                local actions = require("snacks.explorer.actions")
+                tree:open(dir)
+                tree:refresh(dir)
+                actions.update(picker, { target = item_path })
+                picker:focus()
+              end)
+            end,
+          },
         },
       },
     },
